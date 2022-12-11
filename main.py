d000847fb5f1effe8ff1cd968e38fa384c0d7c04
MohammedDiaw <114962567+MohammedDiaw@users.noreply.github.com>
2022-12-11 01:18:39 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+print("welcome to my food quiz! ")
+
+playing = input(" Do you want to play? ")
+
+if playing == "yes":
+    print("Welcome, lets play this game")
+else:
+    print("Goodbye!")
+    quit()
+
+#ice cream
+answer = input("What is the bes flavor of ice cream? ")
+if answer == "cookies and cream":
+    print("correct!")
+else:
+    print("wrong!")
+
+#coffee or tea
+answer = input("what is better, Coffee or Tea? ")
+if answer == "tea":
+    print("A wise choice")
+else:
+    print("I'm dissapointed, smh ")
+    quit()
+
+#Sprite soda
+answer = input("Is Sprite cranberry good? ")
+if answer == "no":
+    print("Good")
+else:
+    print("That utterly disgusting, if I had the vomit emoji I would insert it here ")
+    quit()
+
+#pizza or buger
+answer = input("what is better, pizza or buger? ")
+if answer == "pizza":
+    print("A wise choice")
+else:
+    print("valid ")
+
+#water
+answer = input("is water good ")
+if answer == "yes":
+    print("Correct")
+else:
+    print("Wrong, Goodbye")
+    quit()
+
+print("Thanks for playing! Have a good day")
